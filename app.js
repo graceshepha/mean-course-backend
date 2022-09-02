eab6921030434f5ecf71d1f56ac3d7eb7344d11f
Schephatia <e1859449@cmaisonneuve.qc.ca>
2022-09-02 12:29:12 -0400

    Adding a delete route

--- app.js
+++ app.js
@@ -1,36 +1,52 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const Post = require("./models/post");
+
 const app = express();
+
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch(() => {
+    console.log("Connection failed");
+  });
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/api/posts', (req, res) => {
-  const post = req.body;
+app.post("/api/posts", (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  post.save();
   console.log(post);
   res.status(201).json({
-    message: 'Post added successfully'
+    message: "Post added successfully",
   });
 });
 
-app.get('/api/posts',(req, res, next) => {
-  const posts = [
-    { 
-      id: 'jiofwdwjnq', 
-      title: 'First server-side post',
-      content: 'This is coming from the server'
-    },
-    { 
-      id: 'jiofwdwjnq', 
-      title: 'Second server-side post',
-      content: 'This is the second post content coming from the server'
-    }
+app.get("/api/posts", (req, res, next) => {
+  Post.find().then((documents) => {
+    res.status(200).json({
+      message: "Fetched well",
+      posts: documents,
+    });
+  });
+});
 
-  ]
-  res.status(200)
-  .json({message: 'Fetched well', posts: posts});
+app.delete("/api/posts/:id", (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then(result => {
+    console.log(result);
+    res.status(200).json({ message: "Post deleted" });
+  });
 });
 
 module.exports = app;
