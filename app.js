96235fb79467aaecc647ab7a71f88643e2726ea5
Schephatia <e1859449@cmaisonneuve.qc.ca>
2022-09-14 10:28:48 -0400

    Enhancing router

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const Post = require("./models/post");
+const postsRoutes = require('./routes/posts');
 
 const app = express();
 
@@ -20,33 +20,7 @@ mongoose
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/api/posts',postsRoutes);
 
-app.post("/api/posts", (req, res) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-  });
-  post.save();
-  console.log(post);
-  res.status(201).json({
-    message: "Post added successfully",
-  });
-});
-
-app.get("/api/posts", (req, res, next) => {
-  Post.find().then((documents) => {
-    res.status(200).json({
-      message: "Fetched well",
-      posts: documents,
-    });
-  });
-});
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then(result => {
-    console.log(result);
-    res.status(200).json({ message: "Post deleted" });
-  });
-});
 
 module.exports = app;
